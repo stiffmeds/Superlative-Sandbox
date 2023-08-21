7245c46482f80884e3209590d21a02c824cd34eb
stiffmeds <6158160+stiffmeds@users.noreply.github.com>
2023-08-21 21:49:24 +0800

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,56 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEngine.Networking;
+using UnityEngine;
+using TMPro;
+
+namespace SuperlativeSandbox
+{
+    [BepInPlugin("com.meds.superlativesandbox", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new("com.meds.superlativesandbox");
+        internal static ManualLogSource Log;
+        private void Awake()
+        {
+            // Plugin startup logic
+            Log = Logger;
+            Logger.LogInfo($"Superlative Sandbox activated!");
+            harmony.PatchAll();
+        }
+    }
+    [HarmonyPatch]
+    public class SandEVERYWHERE
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(SandboxManager), "InitCombos")]
+        public static bool InitCombosPrefix(ref SandboxManager __instance)
+        {
+            Traverse.Create(__instance).Field("keyValue").SetValue(new Dictionary<string, List<int>>());
+            Traverse.Create(__instance).Field("currentValue").SetValue(new Dictionary<string, int>());
+            Traverse.Create(__instance).Field("defaultValue").SetValue(new Dictionary<string, int>());
+            Traverse.Create(__instance).Field("comboValues").SetValue(new Dictionary<string, TMP_Text>());
+            Traverse.Create(__instance).Field("valueIsPercent").SetValue(new List<string>());
+            Traverse.Create(__instance).Field("showPositiveSign").SetValue(new List<string>());
+            MethodInfo setCombo = __instance.GetType().GetMethod("SetCombo", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            setCombo.Invoke(__instance, new object[] { "sbEnergy", 0, -100, 100, 1, true, false });
+            setCombo.Invoke(__instance, new object[] { "sbSpeed", 0, -100, 100, 1, true, false });
+            setCombo.Invoke(__instance, new object[] { "sbGold", 0, -100000, 500000, 2500, true, false });
+            setCombo.Invoke(__instance, new object[] { "sbShards", 0, -100000, 500000, 2500, true, false });
+            setCombo.Invoke(__instance, new object[] { "sbCraftCost", 0, -100, 3000, 25, true, true });
+            setCombo.Invoke(__instance, new object[] { "sbUpgradeCost", 0, -100, 3000, 25, true, true });
+            setCombo.Invoke(__instance, new object[] { "sbTransformCost", 0, -100, 3000, 25, true, true });
+            setCombo.Invoke(__instance, new object[] { "sbRemoveCost", 0, -100, 3000, 25, true, true });
+            setCombo.Invoke(__instance, new object[] { "sbEquipmentCost", 0, -100, 3000, 25, true, true });
+            setCombo.Invoke(__instance, new object[] { "sbPetsCost", 0, -100, 3000, 25, true, true });
+            setCombo.Invoke(__instance, new object[] { "sbDivinationCost", 0, -100, 3000, 25, true, true });
+            setCombo.Invoke(__instance, new object[] { "sbMonstersHP", 0, -90, 3000, 10, true, true });
+            setCombo.Invoke(__instance, new object[] { "sbMonstersDamage", 0, -90, 3000, 10, true, true });
+            return false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+# Superlative Sandbox
+
+Increased limits for sandbox mode:
+* Starting energy min: -10 to -100
+* Starting energy max: 10 to 100
+* Hero speed min: -20 to -100
+* Hero speed max: 20 to 100
+* Starting gold min: -10,000 to -100,000
+* Starting gold max: 50,000 to 500,000
+* Starting shards min: -10,000 to -100,000
+* Starting shards max: 50,000 to 500,000
+* Card craft cost max: 300% to 3,000%
+* Card remove cost max: 300% to 3,000%
+* Card transform cost max: 300% to 3,000%
+* Card upgrade cost max: 300% to 3,000%
+* Equipment cost max: 300% to 3,000%
+* Pets cost max: 300% to 3,000%
+* Divination cost max: 300% to 3,000%
+* Monster HP min: -75% to -90%
+* Monster HP increment: 25% to 10%
+* Monster HP max: 300% to 3,000%
+* Monster damage min: -75% to -90%
+* Monster damage increment: 25% to 10%
+* Monster damage max: 300% to 3,000%
+
+## Installation
+
+Install [BepInEx](https://across-the-obelisk.thunderstore.io/package/BepInEx/BepInExPack_AcrossTheObelisk/).
+
+Download the [latest release](https://github.com/stiffmeds/Superlative-Sandbox) and put it in your BepInEx _plugins_ folder.
+
+## Support
+
+Raise a Github issue or ping @stiffmeds in the #modding channel of the [official Across the Obelisk Discord](https://discord.gg/across-the-obelisk-679706811108163701).
+
+## Donations
+
+I make mods because I love it, not because I want to make money from it. If you really, really want to donate to me, my preferred non-profit organisations are [Greyhound Adoptions WA](https://greyhoundadoptionswa.com.au/donation/) ([paypal](https://www.paypal.com/donate?token=m8DwEGGEH0FFsS6PS-5p4MX9_5g8_ocMMrNFjaELN-xcG6Ok-KCFabu5xtB-57QBiOM7QLSuKVUepvL_)) or [Headache Australia](https://headacheaustralia.org.au/donate/).
